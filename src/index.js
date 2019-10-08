2d833e8d9fd6c627e32d1b1d3f89509d80358737
NikolayShishkin <r58korin@gmail.com>
2019-10-08 21:48:55 +0300

    feat: task start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const splitExpression = expression.split('*');
+  const factorialsArr = [];
+  const doubleFactorialsArr = [];
+  for (let i = 0; i < splitExpression.length; i += 1){
+    if(splitExpression[i].endsWith('!!')){
+      doubleFactorialsArr.push(+splitExpression[i].slice(0,-2));
+    } else {
+      factorialsArr.push(+splitExpression[i].slice(0, -1));
+    }
+  }
 }
